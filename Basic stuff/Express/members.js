0582147fb1d5720d3feef7d94dc5bec1d72c9ef8
AlekseyZiuzko <ziuzko.aleksey@gmail.com>
2020-05-13 13:05:55 +0300

    Some updates

---
+++ Basic stuff/Express/members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    }, 
+    {
+        id: 2,
+        name: 'Bob',
+        email: 'bob@gmail.com',
+        status: 'inactive'
+    }, 
+    {
+        id: 3,
+        name: 'Aleksey',
+        email: 'az@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
